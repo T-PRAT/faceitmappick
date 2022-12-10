24a9dca595cee92c1b8c6f4be0d74a402a022d86
tprat <Titouan.p@hotmail.fr>
2022-12-10 15:24:20 +0100

    api work

--- pages/match/[id].jsx
+++ pages/match/[id].jsx
@@ -1,37 +1,22 @@
 import React from 'react'
 
-export default function match({ slug }) {
+export default function match({ data }) {
 	return (
-		<div>{slug}</div>
+		<div>match</div>
 	)
 }
 
-export async function getStaticProps(context) {
-	const slug = context.params.id;
-
-	const res = await fetch("https://open.faceit.com/data/v4/matches/1-493f22c0-5a71-4488-b81e-255ea32df240", { headers: { Authorization: "Bearer " + process.env.FI_API_KEY })
-};
-console.log(res);
-const data = await res.json();
-
-if (!data) {
-	return {
-		notFound: true,
-	};
-}
-console.log(data);
-return {
-	props: {
-		slug: slug,
-		data: data
+export async function getServferSideProps() {
+	const res = await fetch(`https://open.faceit.com/data/v4/matches/1-493f22c0-5a71-4488-b81e-255ea32df240`, {
+		headers: { 'Authorization': 'Bearer ' + process.env.FI_API_KEY }
+	});
+	const data = await res.json();
+	if (!data) {
+		return {
+			notFound: true,
+		};
 	}
-}
-}
-
-export async function getStaticPaths() {
-
 	return {
-		paths: [], //indicates that no page needs be created at build time
-		fallback: 'blocking' //indicates the type of fallback
+		data: data
 	}
 }
